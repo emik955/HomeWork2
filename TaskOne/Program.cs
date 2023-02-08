8372b337d2b0fcc2178ea57d0952403eff2ef9e5
emil.b <emik955@gmail.com>
2023-02-08 17:46:58 +0000

    add TaskOne

--- TaskOne/Program.cs
+++ TaskOne/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+
+Console.WriteLine("Введите трехзначное число:");
+string? digit = Console.ReadLine();
+Console.WriteLine(digit[1]);
